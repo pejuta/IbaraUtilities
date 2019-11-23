f52c259a20b556226526faa4e9992ec73ec3081c
pejuta <sp_dragon3125@hotmail.co.jp>
2019-11-23 12:45:25 +0900

    Update IbaraAddNameToSengens.user.js

--- UserScripts/IbaraAddNameToSengens.user.js
+++ UserScripts/IbaraAddNameToSengens.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name        IbaraAddNameToSengens
-// @namespace   Pejuta
+// @namespace   https://twitter.com/11powder
 // @description イバラシティの宣言確認に名前の表示を追加する
 // @include     http://lisge.com/ib/act_main.php*
 // @include     http://lisge.com/ib/act_trade.php*
 // @include     http://lisge.com/ib/act_battle.php*
 // @include     http://lisge.com/ib/act_skill.php*
-// @version     1.0.0
+// @updateURL   https://pejuta.github.io/IbaraUtilities/UserScripts/IbaraAddNameToSengens.user.js
+// @downloadURL https://pejuta.github.io/IbaraUtilities/UserScripts/IbaraAddNameToSengens.user.js
+// @version     1.0.1
 // @grant       none
 // ==/UserScript==
-(() => {
-	const $ = window.$;
+(($) => {
+	"use strict";
 	$("#SENGENTEXT").prepend(`<b id="NicknameAndENo" class="Y3">${$(".SHIDARI>table:first a:first").text().trim()}</b>`);
-})()
+})(jQuery);
