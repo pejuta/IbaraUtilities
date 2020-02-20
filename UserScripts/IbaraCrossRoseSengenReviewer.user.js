87cc11b510e43dc97f4a63d4e21c032d1e877047
pejuta <sp_dragon3125@hotmail.co.jp>
2020-02-20 21:53:03 +0900

    Update IbaraCrossRoseSengenReviewer.user.js

--- UserScripts/IbaraCrossRoseSengenReviewer.user.js
+++ UserScripts/IbaraCrossRoseSengenReviewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IbaraCrossRoseSengenReviewer
 // @namespace    https://twitter.com/11powder
-// @version      0.1.3
+// @version      0.1.3.1
 // @description  CrossRoseのHomeでの宣言確認を可能にします。
 // @author       pejuta
 // @include      http://lisge.com/ib/act_index.php*
@@ -11,6 +11,47 @@
 // ==/UserScript==
 (($) => {
     "use strict";
+
+    function bindBattle(charactersInfo) {
+        $("[TP='SNBTN']").click(function(){
+          var id = $(this).attr('ID');
+          var bn = $(this).attr('BTNNO');
+          id = id.replace("BTN","HTML");
+          if( $("#"+id).css('display')=='block' ){
+            $("#"+id).hide();
+            $(this).addClass("BUTT"+bn).removeClass("BUTT0");
+          }else{
+            $("#"+id).show();
+            $(this).addClass("BUTT0").removeClass("BUTT1").removeClass("BUTT2");
+          }
+        });
+
+        $("[TP='BTBTN']").click(function(){
+          var bn = $(this).attr('NO');
+          if( $(this).hasClass('BUTT0') ){
+            $(this).addClass("BUTT3").removeClass("BUTT0");
+            $(".BTYPE"+bn).hide();
+          }else{
+            $(this).addClass("BUTT0").removeClass("BUTT3");
+            $(".BTYPE"+bn).show();
+          }
+        });
+
+        $("#SNCHK").one("click", function(){
+            charactersInfo.forEach((item) => {
+                CHKPM(item.eno, item.ano);
+            });
+        });
+
+      function CHKPM(no,ano){
+        $.post("act_battle.php",{ceno:no, a:ano},
+          function(data){
+            $("#CHKHTML"+no+"A"+ano).html(data);
+          }
+        );
+      }
+    }
+
 
     const DATANAME_SENGEN_INDEX = "idx";
     const DATANAME_HAS_SENGEN = "has_sengen";
@@ -46,30 +87,41 @@
                 url = "http://lisge.com/ib/act_skill.php";
                 break;
             default:
-                isAide = true;
-                break;
+                throw new Error("invalid operation");
+                // isAide = true;
+                // break;
         }
 
         if (isAide) {
-            let aideIndex = sengenIndex - 2;
-            let battleHtml = sengenHtmls[2];
-            if (!battleHtml) {
-                await appendSengenText(2);
-                battleHtml = sengenHtmls[2];
-            }
+            return;
+            // let aideIndex = sengenIndex - 2;
+            // let battleHtml = sengenHtmls[2];
+            // if (!battleHtml) {
+            //     await appendSengenText(2);
+            //     battleHtml = sengenHtmls[2];
+            // }
 
-            const $aideAnchor = $(battleHtml, _vdoc).find("a[href^='act_se.php'] > span.Y3").nextUntil(":not(a)").eq(aideIndex);
-            const aideIdMatch = /(?:\?|&)a=(\d+)/.exec($aideAnchor.attr("href"));
-            if (!aideIdMatch) {
-                throw new Error("invalid operation: the aide id of " + $tds.get(0).textContent + " has not found.");
-            }
-            url = "http://lisge.com/ib/act_battle.php?a=" + aideIdMatch[1];
+            // const $aideAnchor = $(battleHtml, _vdoc).find("a[href^='act_se.php'] > span.Y3").nextUntil(":not(a)").eq(aideIndex);
+            // const aideIdMatch = /(?:\?|&)a=(\d+)/.exec($aideAnchor.attr("href"));
+            // if (!aideIdMatch) {
+            //     throw new Error("invalid operation: the aide id of " + $tds.get(0).textContent + " has not found.");
+            // }
+            // url = "http://lisge.com/ib/act_battle.php?a=" + aideIdMatch[1];
         }
 
         const sengenHtml = sengenHtmls[sengenIndex] = await (await fetch(url)).text();
         const $sengenText = $(sengenHtml, _vdoc).find("#SENGENTEXT").show();
         const $sengenContainer = $("<tr class='SengenText'><td colspan=3></td></tr>").hide().insertAfter($tdSengen.closest("tr"));
         $sengenContainer.find("td").append($sengenText);
+
+        if (sengenIndex === 2) {
+            // battle
+            const charactersInfo = $("[TP='SNBTN']").get().map((e) => /^CHKBTN(\d+)A(\d+)$/i.exec(e.id)).filter(x => x).map((x) => {
+                return { eno: x[1], ano: x[2] };
+            });
+            bindBattle(charactersInfo);
+        }
+
         return $sengenContainer;
     }
 
@@ -77,6 +129,8 @@
         const tdSengen = e.nextElementSibling;
         tdSengen.parentElement.dataset[DATANAME_SENGEN_INDEX] = i;
         if (e.innerHTML.indexOf("未宣言") !== -1) {
+            return;
+        } else if (i > 2 && i < sengenCount - 1) {
             return;
         }
         tdSengen.parentElement.dataset[DATANAME_HAS_SENGEN] = true;
